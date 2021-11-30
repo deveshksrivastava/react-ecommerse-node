831bded3fa91b6334fa9d80c390bd86ffec4ab90
satish kumar <53410112+satish122@users.noreply.github.com>
2021-11-30 11:55:32 +0530

    map on home

--- src/Component/Pages/homeContact.js
+++ src/Component/Pages/homeContact.js
@@ -3,7 +3,29 @@ import React from 'react'
 export default function HomeContact() {
     return (
         <div className="ps-home-contact">
-        <div id="contact-map" data-address="New York, NY" data-title="BAKERY LOCATION!" data-zoom="17"> </div>  
+            
+        <div id="contact-map" data-address="New York, NY" data-title="BAKERY LOCATION!" data-zoom="17">
+        <div className="container">
+              <div className="row">
+                <div className="col-sm">
+                  <div className="map-iframe">
+                    <iframe                      
+                      title="map"
+                      height="541.25"
+                      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d317718.69319292053!2d-0.3817765050863085!3d51.528307984912544!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47d8a00baf21de75%3A0x52963a5addd52a99!2sLondon%2C+UK!5e0!3m2!1sen!2spl!4v1562654563739!5m2!1sen!2spl"
+                      style={{
+                        border: 0,
+                        width: '100% ',
+                        height: 500,
+                    
+                        float:'right'
+                      }}
+                    ></iframe>
+                  </div>
+                </div>
+              </div>
+            </div>    
+        </div>  
         <div className="ps-home-contact__form">
         <header>
             <h3>Contact Us</h3>
