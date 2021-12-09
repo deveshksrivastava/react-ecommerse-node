4d10944c367f171af8a13c086c9a241b0d2ea914
satish kumar <53410112+satish122@users.noreply.github.com>
2021-12-09 19:50:13 +0530

    item alignment in home

--- Backend/routes/pool.js
+++ Backend/routes/pool.js
@@ -3,7 +3,7 @@ var pool = mysql.createPool({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "1234",
+  password: "sk123",
   database: "gamezone",
   connectionLimit: 100,
   multipleStatements: true,
