8cde798a3a4ff3cec5c3dbb3937dfe2ec26538cb
satish kumar <53410112+satish122@users.noreply.github.com>
2021-12-11 20:11:38 +0530

    fixing layout

--- src/Component/Pages/home.js
+++ src/Component/Pages/home.js
@@ -596,8 +596,10 @@ const handleQtyChange=(value,item)=>{
                   <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 ">
                     <div class="ps-owl-actions"><a class="ps-prev" href="#"><i class="ps-icon-arrow-right"></i>Prev</a><a class="ps-next" href="#">Next<i class="ps-icon-arrow-left"></i></a></div>
                   </div>
+                  
             </div>
-            <div style={{ display: "flex", flexDirection: "column" }}>
+            
+             <div style={{ display: "flex", flexDirection: "column" }}>
                 <div
                   style={{
                     width: "100%",
@@ -606,7 +608,7 @@ const handleQtyChange=(value,item)=>{
                     alignItems: "center",
                   }}
                 >
-                  {/* <IconButton
+                  <IconButton
                     style={{
                       background: "#1e6b7b",
                       position: "absolute",
@@ -618,13 +620,59 @@ const handleQtyChange=(value,item)=>{
                     <ArrowBackIosIcon
                       style={{ color: "#FFF", fontSize: "large" }}
                     />
-                  </IconButton> */}
+                  </IconButton>
                   <div style={{ width: "98%" }}>
-                    <Slider {...itemsettings}> {showOffers()}
-                    
+                    <Slider {...itemsettings}> 
+                          {/* {showOffers()} */}
+                          {listSubOffers.map((item, index) => {
+                        return (
+                          <div
+                            className="grid-item kids"
+                            // style={{ padding: "5px", display: "flex" }}
+                          >
+                            <div className="grid-item__content-wrapper">
+                              <div className="ps-shoe mb-30">
+                                <div class="ps-shoe">
+                                  <div class="ps-shoe__thumbnail">
+                                    <div className="ps-badge">
+                                      <span>New</span>
+                                    </div>
+                                    <div className="ps-badge ps-badge--sale ps-badge--2nd">
+                                      <span>-35%</span>
+                                    </div>
+                                    <a class="ps-shoe__favorite" href="#">
+                                      <i class="ps-icon-heart"></i>
+                                    </a>
+                                    <img src={`${ServerURL}/images/${item.icon}`} alt="FNF" width="auto" height='160px' onClick={()=>props.history.push({"pathname":"/productView"},{'product':item})}/>
+                                    {/* <a class="ps-shoe__overlay" href="product-detail.html"></a> */}
+                                  </div>
+                                  <div class="ps-shoe__content">
+                                    {/* <div class="ps-shoe__variants">
+                                      <div class="ps-shoe__variant normal"><img src="images/shoe/2.jpg" alt=""/><img src="images/shoe/3.jpg" alt=""/><img src="images/shoe/4.jpg" alt=""/><img src="images/shoe/5.jpg" alt=""/></div>
+                                      <select class="ps-rating ps-shoe__rating">
+                                        <option value="1">1</option>
+                                        <option value="1">2</option>
+                                        <option value="1">3</option>
+                                        <option value="1">4</option>
+                                        <option value="2">5</option>
+                                      </select>
+                                    </div> */}
+                                    <div class="ps-shoe__detail"><a class="ps-shoe__name" href="product-detai.html">{item.subcategoryname.length <= 20
+                                          ? item.subcategoryname.toUpperCase()
+                                          : item.subcategoryname.toUpperCase().substring(0, 18) + ".."}</a>
+                                      {/* <p class="ps-shoe__categories"><a href="#">Men shoes</a>,<a href="#"> Nike</a>,<a href="#"> Jordan</a></p><span class="ps-shoe__price"> £ {item.price}</span> */}
+                                    </div>
+                                  </div>
+                                </div>
+
+                              </div>
+                            </div>
+                          </div>
+                        );
+                      })}
                     </Slider>
                   </div>
-                  {/* <IconButton
+                   <IconButton
                     style={{
                       background: "#1e6b7b",
                       position: "absolute",
@@ -636,9 +684,9 @@ const handleQtyChange=(value,item)=>{
                     <ArrowForwardIosIcon
                       style={{ color: "#FFF", fontSize: "large" }}
                     />
-                  </IconButton> */}
+                  </IconButton> 
                 </div>
-              </div>
+              </div> 
           </div>
           {/* {listMobiles.map((item, index) => {
                               return ( 
