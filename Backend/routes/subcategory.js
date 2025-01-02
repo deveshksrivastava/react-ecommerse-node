d19c0a48302256a33b36437a9201bc16e41d799e
Devesh Srivastava <devesh.kr.sri@gmail.com>
2025-01-02 10:44:14 +0000

    fixing bugs and updating readme

--- Backend/routes/subcategory.js
+++ Backend/routes/subcategory.js
@@ -1,60 +1,58 @@
 var express = require('express');
 var router = express.Router();
-var pool=require('./pool.js')
-var upload=require('./multer');
+var pool = require('./pool.js')
+var upload = require('./multer.js');
 
 /* GET home page. */
-router.post('/addnewsubcategory',upload.any(), function(req, res, next) {
-    
-   var sql="insert into subcategory(categoryid,subcategoryname,description,icon,ad) values(?,?,?,?,?)";
-    pool.query(sql,
-    [req.body.categoryid,
-    req.body.subcategoryname,
-    req.body.description,
-//    req.body.price,
-    req.files[0].originalname,
-    req.files[1].originalname,
- //   req.body.adstatus,
-   // req.body.stock,
-   // req.body.offer,
-    ],
-    function(error,result){
-        if(error)
-        { console.log(error)
-            console.log(sql);
-            res.status(500).json({result:false})
-        }
-        else{
+router.post('/addnewsubcategory', upload.any(), function (req, res, next) {
 
-            res.status(200).json({result:true})
-        }
-    })
+    var sql = "insert into subcategory(categoryid,subcategoryname,description,icon,ad) values(?,?,?,?,?)";
+    pool.query(sql,
+        [req.body.categoryid,
+        req.body.subcategoryname,
+        req.body.description,
+        //    req.body.price,
+        req.files[0].originalname,
+        req.files[1].originalname,
+            //   req.body.adstatus,
+            // req.body.stock,
+            // req.body.offer,
+        ],
+        function (error, result) {
+            if (error) {
+                console.log(error)
+                console.log(sql);
+                res.status(500).json({ result: false })
+            }
+            else {
+
+                res.status(200).json({ result: true })
+            }
+        })
 
 });
 
-router.post('/displaysubcategorybycategoryid',function(req,res){
+router.post('/displaysubcategorybycategoryid', function (req, res) {
 
-    pool.query("select * from subcategory where categoryid=?",[req.body.categoryid], function(error,result){
-        if(error)
-        {
+    pool.query("select * from subcategory where categoryid=?", [req.body.categoryid], function (error, result) {
+        if (error) {
             console.log(error)
             res.status(500).json([])
         }
-        else{
+        else {
             res.status(200).json(result)
         }
     })
 })
 
-router.post('/displayallsubcategorybycategoryid',function(req,res){
+router.post('/displayallsubcategorybycategoryid', function (req, res) {
 
-    pool.query("select * from subcategory where categoryid=?",[req.body.categoryid], function(error,result){
-        if(error)
-        {
+    pool.query("select * from subcategory where categoryid=?", [req.body.categoryid], function (error, result) {
+        if (error) {
             console.log(error)
             res.status(500).json([])
         }
-        else{
+        else {
             res.status(200).json(result)
         }
     })
@@ -62,88 +60,86 @@ router.post('/displayallsubcategorybycategoryid',function(req,res){
 
 
 
-router.get('/displayall',function(req,res){
+router.get('/displayall', function (req, res) {
 
-    pool.query("select * from subcategory", function(error,result){
-        if(error)
-        {
+    pool.query("select * from subcategory", function (error, result) {
+        if (error) {
             res.status(500).json([])
         }
-        else{
+        else {
             res.status(200).json(result)
         }
     })
 })
-router.post('/editicon',upload.single('icon'), function(req, res, next) {
+router.post('/editicon', upload.single('icon'), function (req, res, next) {
 
     pool.query("update subcategory set icon=? where subcategoryid=?",
-    [   req.file.originalname,
+        [req.file.originalname,
         req.body.subcategoryid]
-    ,function(error,result){
-        if(error)
-        { console.log(error)
-            res.status(500).json({result:false})
-        }
-        else{
+        , function (error, result) {
+            if (error) {
+                console.log(error)
+                res.status(500).json({ result: false })
+            }
+            else {
 
-            res.status(200).json({result:true})
-        }
-    })
+                res.status(200).json({ result: true })
+            }
+        })
 
 });
 
-router.post('/editad',upload.single('ad'), function(req, res, next) {
+router.post('/editad', upload.single('ad'), function (req, res, next) {
 
     pool.query("update subcategory set ad=? where subcategoryid=?",
-    [   req.file.originalname,
+        [req.file.originalname,
         req.body.subcategoryid]
-    ,function(error,result){
-        if(error)
-        { console.log(error)
-            res.status(500).json({result:false})
-        }
-        else{
+        , function (error, result) {
+            if (error) {
+                console.log(error)
+                res.status(500).json({ result: false })
+            }
+            else {
 
-            res.status(200).json({result:true})
-        }
-    })
+                res.status(200).json({ result: true })
+            }
+        })
 
 });
 
-router.post('/editsubcategorydata', function(req, res, next) {
+router.post('/editsubcategorydata', function (req, res, next) {
     pool.query("update subcategory set categoryid=?,subcategoryname=?,description=? where subcategoryid=?",
-    [req.body.categoryid,
-    req.body.subcategoryname,
-    req.body.description,
-    req.body.subcategoryid]
-    ,function(error,result){
-        if(error)
-        {
-            console.log(error)
-            res.status(500).json({result:false})
-        }
-        else{
-            console.log(req.body)
-            res.status(200).json({result:true})
-        }
-    })
+        [req.body.categoryid,
+        req.body.subcategoryname,
+        req.body.description,
+        req.body.subcategoryid]
+        , function (error, result) {
+            if (error) {
+                console.log(error)
+                res.status(500).json({ result: false })
+            }
+            else {
+                console.log(req.body)
+                res.status(200).json({ result: true })
+            }
+        })
 
 
 });
-router.post('/deletesubcategory', function(req, res, next) {
+router.post('/deletesubcategory', function (req, res, next) {
 
     pool.query("delete from subcategory where subcategoryid=?",
-    [req.body.subcategoryid]
-    ,function(error,result){
-        if(error)
-        { console.log(error)
-            res.status(500).json({result:false})
-        }
-        else{
+        [req.body.subcategoryid]
+        , function (error, result) {
+            if (error) {
+                console.log(error)
+                res.status(500).json({ result: false })
+            }
+            else {
 
-            res.status(200).json({result:true})
-        }
-    })
+                res.status(200).json({ result: true })
+            }
+        })
 
 });
 /*
@@ -160,26 +156,24 @@ router.get('/subcategoryoffers',function(req,res){
     })
 })
 */
-router.get('/subcategoryoffers',function(req,res){
+router.get('/subcategoryoffers', function (req, res) {
 
-    pool.query("select * from subcategory where categoryid=5", function(error,result){
-        if(error)
-        {
-            res.status(500).json({result:false})
+    pool.query("select * from subcategory where categoryid=5", function (error, result) {
+        if (error) {
+            res.status(500).json({ result: false })
         }
-        else{
+        else {
             res.status(200).json(result)
         }
     })
 })
-router.get('/subcategoryoffers1',function(req,res){
+router.get('/subcategoryoffers1', function (req, res) {
 
-    pool.query("select * from subcategory where categoryid=6", function(error,result){
-        if(error)
-        {
-            res.status(500).json({result:false})
+    pool.query("select * from subcategory where categoryid=6", function (error, result) {
+        if (error) {
+            res.status(500).json({ result: false })
         }
-        else{
+        else {
             res.status(200).json(result)
         }
     })
